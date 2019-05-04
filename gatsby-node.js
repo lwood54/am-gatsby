b826d146163b40b527bd79bddf1a282a260ce545
Logan Wood <lwood3499@gmail.com>
2019-05-04 09:34:27 -0500

    dynamically create slugs to help autgenerate pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,18 @@
+const path = require("path");
+
 module.exports.onCreateNode = ({ node, actions }) => {
       const { createNodeField } = actions;
 
       if (node.internal.type === "MarkdownRemark") {
-            console.log("markdown nodes: ", JSON.stringify(node, undefined, 4));
+            // use path to access just the file name from the absolute path
+            // use second argument to remove the file extension type
+            // then we can use that to dynamically create new pages
+            const slug = path.basename(node.fileAbsolutePath, ".md");
+
+            createNodeField({
+                  node,
+                  name: "slug",
+                  value: slug
+            });
       }
 };
